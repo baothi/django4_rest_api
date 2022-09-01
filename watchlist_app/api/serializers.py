adc00e55f669c4a59efc47fd88e423950a5f018e
baothi <baothi246@gmail.com>
2022-09-01 12:54:49 +0700

    fix api

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,32 +1,63 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
-
-# https://www.django-rest-framework.org/tutorial/1-serialization/#creating-a-serializer-class
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    description = serializers.CharField()
-    active = serializers.BooleanField()
-
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
-
+# https://github.com/encode/django-rest-framework/blob/master/rest_framework/serializers.py
+class MovieSerializer(serializers.ModelSerializer):
+    # https://www.django-rest-framework.org/api-guide/fields/#serializermethodfield
+    len_name = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Movie
+        fields = "__all__"
+        # fields = ['id', 'name', 'description']
+        # exclude = ["active"]
+    
     def validate(self, data):
         if data['name'] == data['description']:
             raise serializers.ValidationError("Title and Description should be different!")
         else:
             return data
-
+    
+    def get_len_name(self, objects):
+        return len(objects.name)
+    
     def validate_name(self, value):
         if len(value) < 2:
             raise serializers.ValidationError("Name is too short!")
         else:
             return value
+
+
+# # https://www.django-rest-framework.org/api-guide/serializers/#validators
+# def name_length(value):
+#     if len(value) < 2:
+#         raise serializers.ValidationError("Name is too short!")
+
+# # https://www.django-rest-framework.org/tutorial/1-serialization/#creating-a-serializer-class
+# class MovieSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField(validators=[name_length])
+#     description = serializers.CharField()
+#     active = serializers.BooleanField()
+
+#     def create(self, validated_data):
+#         return Movie.objects.create(**validated_data)
+
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.active = validated_data.get('active', instance.active)
+#         instance.save()
+#         return instance
+
+#     def validate(self, data):
+#         if data['name'] == data['description']:
+#             raise serializers.ValidationError("Title and Description should be different!")
+#         else:
+#             return data
+
+#     # def validate_name(self, value):
+#     #     if len(value) < 2:
+#     #         raise serializers.ValidationError("Name is too short!")
+#     #     else:
+#     #         return value
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,34 +1,35 @@
 import re
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+# from rest_framework.decorators import api_view
 
 # https://www.django-rest-framework.org/api-guide/status-codes/
 from rest_framework import status
 
+from rest_framework.views import APIView
+
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 
-
-@api_view(['GET', 'POST'])
-def movie_list(request):
-    if request.method == 'GET':
+class MovieListAV(APIView):
+    
+    # https://www.django-rest-framework.org/api-guide/views/
+    def get(self, request):
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies,many=True)
         return Response(serializer.data)
     
-    if request.method == 'POST':
+    def post(self, request):
         serializer = MovieSerializer(data=request.data)
+        # https://www.django-rest-framework.org/api-guide/serializers/#validation
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
 
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def movie_details(request, pk):
+class MovieDetailAV(APIView):
     
-    if request.method == 'GET':
+    def get(self, request, pk): 
         try:
             movie = Movie.objects.get(pk=pk)
         except Movie.DoesNotExist:
@@ -36,7 +37,7 @@ def movie_details(request, pk):
         serializer = MovieSerializer(movie)
         return Response(serializer.data)
     
-    if request.method == 'PUT':
+    def put(self, request,pk):
         movie = Movie.objects.get(pk=pk)
         serializer = MovieSerializer(movie,data=request.data)
         if serializer.is_valid():
@@ -44,10 +45,51 @@ def movie_details(request, pk):
             return Response(serializer.data)
         else:
             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-            
     
-    
-    if request.method == 'DELETE':
+    def delete(self, request,pk):
         movie = Movie.objects.get(pk=pk)
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+# @api_view(['GET', 'POST'])
+# def movie_list(request):
+#     if request.method == 'GET':
+#         movies = Movie.objects.all()
+#         serializer = MovieSerializer(movies,many=True)
+#         return Response(serializer.data)
+    
+#     if request.method == 'POST':
+#         serializer = MovieSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
+
+
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def movie_details(request, pk):
+    
+#     if request.method == 'GET':
+#         try:
+#             movie = Movie.objects.get(pk=pk)
+#         except Movie.DoesNotExist:
+#             return Response({'error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
+#         serializer = MovieSerializer(movie)
+#         return Response(serializer.data)
+    
+#     if request.method == 'PUT':
+#         movie = Movie.objects.get(pk=pk)
+#         serializer = MovieSerializer(movie,data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            
+    
+    
+#     if request.method == 'DELETE':
+#         movie = Movie.objects.get(pk=pk)
+#         movie.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
